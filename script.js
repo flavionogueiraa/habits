43cfd9e628588d10259290863bc38b514ff32899
flaviotech <flavio.nogueira.profissional@gmail.com>
2023-01-19 21:12:37 -0300

    Aula 04

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 const form = document.querySelector("form");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data = {
-  run: ["01-01", "01-02", "01-06"],
-  journal: ["01-02"],
-  takePills: ["01-03"],
-};
+button.addEventListener("click", add);
+form.addEventListener("change", save);
 
-nlwSetup.setData(data);
-nlwSetup.load();
+function add() {
+  const today = new Date().toLocaleDateString("pt-BR").slice(0, 5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Day already exists");
+    return;
+  }
+
+  nlwSetup.addDay(today);
+}
+
+function save() {
+  const data = nlwSetup.data;
+  const stringifyData = JSON.stringify(data);
+  localStorage.setItem("NLWSetup@habits", stringifyData);
+}
+
+function setAllDataOnInit() {
+  const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
+  nlwSetup.setData(data);
+  nlwSetup.load();
+}
+
+(function init() {
+  setAllDataOnInit();
+})();
